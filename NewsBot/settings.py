f52d0ce5053f401834f5ec30dfccaaac5961f9ab
KV <kumaravelu2003@gmail.com>
2025-08-30 13:29:09 +0530

    Prepare for Render deployment

--- NewsBot/settings.py
+++ NewsBot/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.versel.app', ]
+ALLOWED_HOSTS = ['.versel.app','newsbot-gzlm.onrender.com' ]
 
 
 # Application definition
